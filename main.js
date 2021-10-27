3d82aea15e4e9d468e2c4bb135dd6c8d0cbb38ec
Zarif-Syed <93282958+Zarif-Syed@users.noreply.github.com>
2021-10-27 13:54:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function color(){
+    document.getElementById("a").style.color="red";
+}
+function color1(){
+    document.getElementById("b").style.color="yellow";
+}
+function font(){
+    document.getElementById("c").style.fontSize="30px";
+}
